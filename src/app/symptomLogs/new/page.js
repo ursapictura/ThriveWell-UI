e800be484f0297a6605341d5c45d2d6386187984
ursapictura <hsmithfreelance@gmail.com>
2024-12-15 20:27:54 -0600

    improved layout of SymptomLogForm.

--- src/app/symptomLogs/new/page.js
+++ src/app/symptomLogs/new/page.js
@@ -3,8 +3,8 @@ import SymptomLogForm from '../../../components/forms/SymptomLogForm';
 
 export default function AddSymptomLog() {
   return (
-    <div>
-      <h3>Experience a symptom? Log it here!</h3>
+    <div className="logFormContainer">
+      <h2 style={{ textAlign: 'center' }}>Experience a symptom? Log it here!</h2>
       <SymptomLogForm />
     </div>
   );
